b1114a7c8cb6be4916805edf729cceef772e997a
Pierre <jpcampoverde9801@hotmail.com>
2022-07-09 13:07:23 -0300

    Added Google Analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 const Document = () => {
   return (
     <Html lang="es">
-      <Head />
+      <Head>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-C1VGG4CQ40"
+        ></Script>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-C1VGG4CQ40');`}
+        </Script>
+      </Head>
       <body>
         <Main />
         <NextScript />
