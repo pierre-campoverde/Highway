9bd209ebbddefb227821380939db3d5559a06520
Pierre <jpcampoverde9801@hotmail.com>
2022-08-10 12:51:53 -0300

    Security Header removed

--- next.config.js
+++ next.config.js
@@ -5,20 +5,20 @@ const nextConfig = {
   images: {
     domains: ["ucarecdn.com"],
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'self'; script-src *; img-src *; style-src: *; frame-src *.google.com;",
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/(.*)",
+  //       headers: [
+  //         {
+  //           key: "Content-Security-Policy",
+  //           value:
+  //             "default-src 'self'; script-src *; img-src *; style-src: *; frame-src *.google.com;",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
