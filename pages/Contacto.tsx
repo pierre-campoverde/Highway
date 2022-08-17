184aea4345bf8ebb7f4bd2b046823ca20a2c888b
Pierre <jpcampoverde9801@hotmail.com>
2022-08-17 17:16:37 -0300

    Google Iframe Changed

--- pages/Contacto.tsx
+++ pages/Contacto.tsx
@@ -14,7 +14,7 @@ const Contacto: NextPage = () => {
         <Contact />
         <section className="container mx-auto my-10">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.6643753685285!2d-86.85198198506706!3d21.046111085989033!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc86bfe7e605f8480!2zMjHCsDAyJzQ2LjAiTiA4NsKwNTAnNTkuMyJX!5e0!3m2!1sen!2sar!4v1657842358192!5m2!1sen!2sar"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.6644200315236!2d-86.84977730000001!3d21.046109299999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f4e817cd195e733%3A0xe05fc507ac2f7562!2sHighway%20Car%20Rental!5e0!3m2!1sen!2sar!4v1660767229539!5m2!1sen!2sar"
             width="100%"
             height="450"
             className="border-none"
