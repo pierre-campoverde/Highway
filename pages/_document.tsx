53fa7cae53c48096d913741830622f16692877de
Pierre <jpcampoverde9801@hotmail.com>
2022-08-25 14:09:52 -0300

    Removed pixels script to push content changes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,28 +18,6 @@ const Document = () => {
 
   gtag('config', 'G-C5LPSE2YH1');`}
         </Script>
-
-        <Script id="facebook-pixel">
-          {`!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1260464071388945');
-fbq('track', 'PageView');`}
-        </Script>
-        <noscript>
-          <Image
-            height="1"
-            width="1"
-            alt=""
-            className="hidden"
-            src="https://www.facebook.com/tr?id=1260464071388945&ev=PageView&noscript=1"
-          />
-        </noscript>
       </Head>
       <body>
         <Main />
