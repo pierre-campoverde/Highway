b648a7023b604286c09f1963f2926b2c75913609
Pierre <jpcampoverde9801@hotmail.com>
2022-10-31 23:53:14 -0300

    Added translations to about us section

--- components/Templates/Home/AboutUs.tsx
+++ components/Templates/Home/AboutUs.tsx
@@ -1,18 +1,19 @@
 import Link from "next/link";
 import React from "react";
+import { useIntl } from "react-intl";
 
 const AboutUs = () => {
+  const intl = useIntl();
   return (
     <section className="w-full bg-AboutUsImage bg-scroll bg-cover bg-center bg-no-repeat text-center py-6  my-10">
       <div className="md:w-6/12 mx-auto py-4">
         <p className="font-gillSansRegular text-3xl text-white">
-          “Dirigiremos nuestra industria al defender la excelencia del servicio
-          y forjando una lealtad incomparable de parte de los clientes”.
+          {intl.formatMessage({ id: "page.home.aboutUs.description" })}
         </p>
         <div className="w-44 my-3 mx-auto">
           <Link href={"/AboutUs"}>
             <a className="border-2 mt-2 block text-xl px-2 py-3  text-white rounded-xl hover:bg-white hover:text-customRed transition-all">
-              Sobre Nosotros
+              {intl.formatMessage({ id: "page.home.aboutUs.cta" })}
             </a>
           </Link>
         </div>
