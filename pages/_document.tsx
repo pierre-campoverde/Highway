12a55f0ee814d8d4af6affc7a3342e2f7af3a04f
pierrecampoverde <pierrecampoverde@gmail.com>
2023-10-24 13:28:19 -0300

    Moved google nonscript to body

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -26,16 +26,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-W72GHGGR');`}
         </Script>
-        {/* <!-- Google Tag Manager (noscript) -->*/}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-W72GHGGR"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
-        </noscript>
-        {/* <!-- End Google Tag Manager (noscript) --> */}
+       
         <noscript>
           <img
             height="1"
@@ -46,6 +37,14 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         </noscript>
       </Head>
       <body>
+      <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W72GHGGR"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <Main />
         <NextScript />
       </body>
